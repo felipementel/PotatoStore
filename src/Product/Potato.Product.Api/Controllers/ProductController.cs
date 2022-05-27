083f298d4d2f69e3bd6dbff2bfa459760b8fe047
Felipe Augusto <c0624460@vale.com>
2022-05-26 22:05:39 -0300

    feat: initial commit in Api Controller

---
+++ src/Product/Potato.Product.Api/Controllers/ProductController.cs
@@ -0,0 +1,39 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace Potato.Product.Api.Controllers;
+
+[ApiController]
+[ApiVersion("1.0")]
+[ApiVersion("0.9", Deprecated = true)]
+[Route("api/v{version:apiVersion}/[controller]")]
+public class ProductController : ControllerBase
+{
+    private readonly ILogger<ProductController> _logger;
+
+    public ProductController(ILogger<ProductController> logger)
+    {
+        _logger = logger;
+    }
+
+    [HttpGet]
+    [MapToApiVersion("1.0")]
+    [Produces("application/json")]
+    [ProducesResponseType(StatusCodes.Status200OK)]
+    [ProducesResponseType(StatusCodes.Status404NotFound)]
+    [ProducesResponseType(StatusCodes.Status500InternalServerError)]
+    public async Task<IActionResult> GetAll()
+    {
+        return Ok();
+    }
+
+    [HttpPost]
+    [MapToApiVersion("1.0")]
+    [Produces("application/json")]
+    [ProducesResponseType(StatusCodes.Status201Created)]
+    [ProducesResponseType(StatusCodes.Status404NotFound)]
+    [ProducesResponseType(StatusCodes.Status500InternalServerError)]
+    public async Task<IActionResult> CreateProduct()
+    {
+        return Ok();
+    }
+}
