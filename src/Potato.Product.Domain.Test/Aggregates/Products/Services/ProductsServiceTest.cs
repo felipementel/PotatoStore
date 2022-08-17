fc8bc13ce21c3d25c2216745d7f62637b4e10a1e
Edson <ferreiraspe@gmail.com>
2022-08-17 00:33:24 -0300

    test: implementing test GetAll ProductService

---
+++ src/Potato.Product.Domain.Test/Aggregates/Products/Services/ProductsServiceTest.cs
@@ -0,0 +1,56 @@
+﻿using FluentAssertions;
+using Moq;
+using Potato.Product.Domain.Aggregates.Products.Interfaces.Repositories;
+using Potato.Product.Domain.Aggregates.Products.Services;
+
+namespace Potato.Product.Domain.Test.Aggregates.Products.Services
+{
+    public class ProductsServiceTest
+    {
+        private readonly Mock<IProductRepository> _productRepository;
+
+        public ProductsServiceTest()
+        {
+            _productRepository = new Mock<IProductRepository>();
+        }
+
+        [Fact]
+        public async Task Should_Returns_Product_List()
+        {
+            //Arrenge
+            var products = new List<Domain.Aggregates.Products.Entities.Product>()
+            {
+                new Domain.Aggregates.Products.Entities.Product(Guid.NewGuid(), "Geladeira", "Geladeira 410L", "SKU012", 3000)
+
+            };
+
+            _productRepository.Setup(x => x.GetAllAsync()).ReturnsAsync(products);
+
+            //Act
+            var result = await new ProductService(_productRepository.Object).GetAllAsync();
+
+            //Assert
+            _productRepository.Verify(x => x.GetAllAsync(), Times.Once());
+            result.ToList().Count.Should().Be(1);
+        }
+
+        [Fact]
+        public async Task Should_Returns_Product_List_Empty()
+        {
+            //Arrenge
+            var products = new List<Domain.Aggregates.Products.Entities.Product>()
+            {
+
+            };
+
+            _productRepository.Setup(x => x.GetAllAsync()).ReturnsAsync(products);
+
+            //Act
+            var result = await new ProductService(_productRepository.Object).GetAllAsync();
+
+            //Assert
+            _productRepository.Verify(x => x.GetAllAsync(), Times.Once());
+            result.ToList().Count.Should().Be(0);
+        }
+    }
+}
