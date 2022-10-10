0629bd1ebdb846d4fe7261cc37747c7711ac4ef6
Felipe Augusto <felipementel@hotmail.com>
2022-10-10 13:05:54 -0300

    feat: create healthcheck

---
+++ src/Potato.Product.Api/Extensions/HealthCheck/HealthCheckExtensions.cs
@@ -0,0 +1,54 @@
+﻿using HealthChecks.UI.Client;
+using Microsoft.AspNetCore.Diagnostics.HealthChecks;
+using Microsoft.Extensions.Diagnostics.HealthChecks;
+
+namespace Potato.Product.Api.Extensions.HealthCheck
+{
+    public static class HealthCheckExtensions
+    {
+        public static void AddHealthCheckExtensions(
+            this IServiceCollection services,
+            IConfiguration configuration)
+        {
+            services
+                .AddHealthChecks()
+                .AddNpgSql(
+                npgsqlConnectionString: configuration["ConnectionString:Database"],
+                healthQuery: "SELECT 1;",
+                name: "postgresql",
+                failureStatus: HealthStatus.Degraded,
+                tags: new string[] { "db", "sql", "postgresql" });
+        }
+
+        public static void AddHealthChecksUIExtensions(
+            this IServiceCollection services)
+        {
+            services
+            .AddHealthChecksUI(setup =>
+            {
+                setup.DisableDatabaseMigrations();
+                // Set the maximum history entries by endpoint that will be served by the UI api middleware
+                setup.MaximumHistoryEntriesPerEndpoint(50);
+                //Only one active request will be executed at a time.
+                //All the excedent requests will result in 429 (Too many requests)
+                setup.SetApiMaxActiveRequests(1);
+                setup.SetEvaluationTimeInSeconds(5); // Configures the UI to poll for healthchecks updates every 5 seconds
+                setup.AddHealthCheckEndpoint("API with Health Checks", "/healthz");
+            }).AddInMemoryStorage();
+        }
+
+        public static void UseHealthChecksExtensions(this WebApplication app)
+        {
+            app.UseHealthChecks("/healthz", new HealthCheckOptions
+            {
+                Predicate = _ => true,
+                ResponseWriter = UIResponseWriter.WriteHealthCheckUIResponse
+            });
+
+            app.UseHealthChecksUI(options => 
+            { 
+                options.UIPath = "/dashboard";
+            });
+        }
+    }
+}
---
+++ src/Potato.Product.Infra.CrossCutting/IoC.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Potato.Product.Application.AppServices;
+using Potato.Product.Application.Interfaces.Services;
+using Potato.Product.Domain.Aggregates.Products.Interfaces.Repositories;
+using Potato.Product.Domain.Aggregates.Products.Interfaces.Services;
+using Potato.Product.Domain.Aggregates.Products.Services;
+using Potato.Product.Infra.Database.Repositories;
+
+namespace Potato.Product.Infra.CrossCutting;
+
+public static class IoC
+{
+    public static void AddDependeciesInjections(this IServiceCollection Services)
+    {
+        //IoC
+        Services.AddScoped<IProductRepository, ProductRepository>();
+        Services.AddScoped<IProductAppService, ProductAppService>();
+        Services.AddScoped<IProductService, ProductService>();
+    }
+
+
+}
